b8be264941bb72ad2ae4f1aeb8e4b6766f67d6e4
jewelazo <gpalominoe@uni.pe>
2022-01-07 23:50:38 +0000

    create category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,5 +14,35 @@ class CategoriesController < ApplicationController
     def new
         @category=Category.new
     end
+
+    # GET /categories/:id/edit
+    def edit
+        @category=Category.find_by(id: params[:id])
+    end
     
+    # POST /categories
+    def create
+        @category=Category.new(category_params)
+        if @category.save
+            redirect_to categories_path
+        else
+            render :new
+        end
+    end
+
+    # PATCH /categories/:id
+    def update
+        @category=Category.find_by(id:params[:id])
+        if @category.update(category_params)
+            redirect_to categories_path
+        else
+            render :edit
+        end
+    end
+
+
+    private
+    def category_params
+        params.require(:category).permit(:name,:description)
+    end
 end
