584dc12575cb169ebb038249fa0edcc7767abbeb
jewelazo <gpalominoe@uni.pe>
2022-01-07 07:14:54 +0000

    add routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,18 @@
 class CategoriesController < ApplicationController
+
+    # GET /categories
+    def index
+        @categories=Category.all
+    end
+
+    # GET /categories/:id
+    def show
+        @category=Category.find_by(id: params[:id])
+    end
+
+    # GET /categories/new
+    def new
+        @category=Category.new
+    end
+    
 end
