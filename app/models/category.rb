41e3b784afa9ba8ac76dc78020a1f2415ab7b07b
jewelazo <gpalominoe@uni.pe>
2022-01-14 03:58:33 +0000

    add cloudinary gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,5 +7,6 @@ class Category < ApplicationRecord
     # Associations
     has_many :photos, dependent: :destroy
     has_many :comments,as: :commentable, dependent: :destroy
+    has_one_attached :cover
 
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,4 +6,5 @@ class Photo < ApplicationRecord
     # Associations
     belongs_to :category, counter_cache: true
     has_many :comments,as: :commentable, dependent: :destroy
+    has_one_attached :image
 end
