74ad31aeb94bf1efcde54e057a34bb4589fea07e
jewelazo <gpalominoe@uni.pe>
2022-01-21 21:08:44 +0000

    complete photo views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,22 @@ class CommentsController < ApplicationController
     # POST /categories/:category_id/comments
     # POST /photos/:photo_id/comments
     def create
-        @commentable=Category.find_by(id: params[:category_id]) if params[:category_id]
-        @commentable=Photo.find_by(id: params[:photo_id]) if params[:photo_id]
+        if params[:category_id]
+            @commentable=Category.find_by(id: params[:category_id])
+            @category=@commentable
+            @comments=@category.comments
+        elsif params[:photo_id]
+            @commentable=Photo.find_by(id: params[:photo_id])
+            @photo=@commentable
+            @comments=@photo.comments
+        end
         @comment_new=@commentable.comments.new(comment_params)
-        @category=@commentable
-        @comments=@category.comments
         respond_to do |format|
             if @comment_new.save
                 # redirect_to @commentable
                 format.html { redirect_to @commentable, notice: "Comment was successfully created." }
             else
-                format.html { render 'categories/show', status: :unprocessable_entity }
+                format.html { render 'photos/show', status: :unprocessable_entity }
             end
         end
     end
